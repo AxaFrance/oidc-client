a786953ae696d9fd0377dc0c81526f411411b833
Guillaume Chervet <guillaume.chervet@axa.fr>
2018-09-25 19:28:12 +0000

    test

--- scripts/npm.js
+++ scripts/npm.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 
-let template = "registry=https://registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}"
+let template = "registry=https://registry.npmjs.org"
 template += `
-always-auth=true`;
+always-auth=true
+//https://registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}`;
 
 template = template.replace('${NPM_AUTH_TOKEN}', process.env.NPM_AUTH_TOKEN)
 
