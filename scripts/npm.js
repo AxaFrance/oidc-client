87fa1c1e55cdabbdc16b295633b95488493f702d
Guillaume Chervet <guillaume.chervet@axa.fr>
2018-09-26 16:15:16 +0000

    'test'

--- scripts/npm.js
+++ scripts/npm.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const template = `
 always-auth=true
 registry=https://registry.npmjs.org
-//https://registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN}
+//registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN}
 `
 
 const npmrcPath = __dirname +'/../.npmrc';
