240fba3188b6ef265dc76fdf7e7a5c5e9d5279d6
Guillaume Chervet <guillaume.chervet@axa.fr>
2018-09-26 16:10:29 +0000

    'test'

--- scripts/npm.js
+++ scripts/npm.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 
-let template = `registry=https://registry.npmjs.org
+const template = `
+always-auth=true
+registry=https://registry.npmjs.org
+//https://registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN}
 `
-template += "//https://registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}";
-
-template = template.replace('${NPM_AUTH_TOKEN}', process.env.NPM_AUTH_TOKEN)
 
 const npmrcPath = __dirname +'/../.npmrc';
 fs.unlinkSync(npmrcPath);
