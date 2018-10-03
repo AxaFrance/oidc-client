b4f20190ca2738012ade0c9e357dcec4961b06d8
Olivier YOUF <olivier.youf@axa.fr>
2018-10-03 11:00:04 +0200

    fix branch test and doc

--- packages/context/src/OidcComponents/Authenticating.component.js
+++ packages/context/src/OidcComponents/Authenticating.component.js
@@ -1,10 +1,10 @@
-import * as React from 'react';
+import * as React from "react";
 
 const Authenticating = () => (
   <div>
     <div className="container">
       <h1>Authentification en cours</h1>
-      <p>Vous allez être redirigé sur la page de login.</p>
+      <p>Vous allez être redirigé sur la page de login</p>
     </div>
   </div>
 );
