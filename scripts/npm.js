9215d617b759a7d3b34c0fb8f8249057798801f6
Guillaume Chervet <guillaume.chervet@axa.fr>
2018-12-05 15:37:06 +0000

    WIP

--- scripts/npm.js
+++ scripts/npm.js
@@ -1,15 +1,22 @@
 const path = require('path');
+var fs = require('fs');
 
+try{
 const args = process.argv;
 console.log(args);
 
 const NPM_TOKEN = args[2];
 
 
-var fs = require('fs');
+
  
 const template = `registry=https://registry.npmjs.org
 //registry.npmjs.org/:_authToken=${NPM_TOKEN}`;
 
+const filePath = path.join(__dirname, '../.npmrc');
+console.log(filePath);
+fs.writeFileSync(filePath  , template, 'utf8');
 
-fs.writeFileSync( path.join(__dirname, '../.npmrc')  , template, 'utf8');
+} catch(ex) {
+    console.error(ex);
+}
