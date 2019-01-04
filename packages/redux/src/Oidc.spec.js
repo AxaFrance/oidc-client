ea6980fc54a9aefd629cb829ece447cf5afa2243
BIRGAND Julien <julien.birgand@axa.fr>
2019-01-04 15:08:40 +0100

    Add unit to check if isEnabled default props works

--- packages/redux/src/Oidc.spec.js
+++ packages/redux/src/Oidc.spec.js
@@ -1,7 +1,22 @@
 // Link.react.test.js
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { OidcBase } from './Oidc';
+import { shallow } from 'enzyme';
+import { loadUser } from 'redux-oidc';
+
+import Oidc, { OidcBase } from './Oidc';
+
+// I used __esModule to mock a module with default and named exports
+// cf: https://github.com/facebook/jest/issues/5579#issuecomment-397406174
+jest.mock('./authenticationService', () => ({
+  __esModule: true,
+  default: jest.fn(),
+  getUserManager: jest.fn()
+}));
+jest.mock('redux-oidc', () => ({
+  OidcProvider: jest.fn(),
+  loadUser: jest.fn()
+}));
 
 describe('redux-fetch.withAuthentication', () => {
   it('Render <Oidc/> correctly', () => {
@@ -12,5 +27,14 @@ describe('redux-fetch.withAuthentication', () => {
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('should call loadUser when component mounts even if isEnabled prop is not set', () => {
+    const wrapper = shallow(<Oidc store={{}} configuration={{}} />);
+    const instance = wrapper.instance();
+
+    instance.componentWillMount();
+
+    expect(loadUser).toHaveBeenCalled();
   });
 });
