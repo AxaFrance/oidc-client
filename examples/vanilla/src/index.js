79006c7e5b9271f47cbfcdbc5876a0a5e8898f9f
Guillaume Chervet <guillaume.chervet@axa.fr>
2019-03-04 15:43:08 +0000

    WIP

--- examples/vanilla/src/index.js
+++ examples/vanilla/src/index.js
@@ -1,4 +1,5 @@
 // 1. Create the button
+
 var button = document.createElement("button");
 button.innerHTML = "Signin";
 
@@ -8,5 +9,15 @@ body.appendChild(button);
 
 // 3. Add event handler
 button.addEventListener("click", function() {
-  window.vanillaOidc.signinRedirect();
+  window.vanillaOidc.init(configuration).then(function(status) {
+    if (status.type !== "callback") {
+      if (!status.user) {
+        window.vanillaOidc.signinRedirect();
+      } else {
+        var span = document.createElement("span ");
+        span.innerHTML = JSON.stringify(status.user);
+        body.appendChild(span);
+      }
+    }
+  });
 });
