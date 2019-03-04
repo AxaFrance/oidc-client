fcfebc6c64cbe326ea053fa87a5681dc85d0a0f7
Guillaume Chervet <guillaume.chervet@axa.fr>
2019-03-04 15:47:44 +0000

    WIP

--- examples/vanilla/src/index.js
+++ examples/vanilla/src/index.js
@@ -1,5 +1,17 @@
-// 1. Create the button
+const configuration = {
+  client_id: "implicit",
+  redirect_uri: "http://localhost:3000/authentication/callback",
+  response_type: "id_token token",
+  post_logout_redirect_uri: "http://localhost:3000/",
+  scope: "openid profile email",
+  authority: "https://demo.identityserver.io",
+  silent_redirect_uri: "http://localhost:3000/authentication/silent_callback",
+  automaticSilentRenew: true,
+  loadUserInfo: true,
+  triggerAuthFlow: true
+};
 
+// 1. Create the button
 var button = document.createElement("button");
 button.innerHTML = "Signin";
 
