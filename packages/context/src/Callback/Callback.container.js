01af43636e2fc5cb99a08aa154b415857493956b
Olivier YOUF <olivier.youf@axa.fr>
2019-03-26 11:54:43 +0100

    wip : callback

--- packages/context/src/Callback/Callback.container.js
+++ packages/context/src/Callback/Callback.container.js
@@ -1,27 +1,39 @@
-import { withRouter } from 'react-router-dom';
-import { compose, withProps, lifecycle, withHandlers, pure } from 'recompose';
+import { withRouter } from "react-router-dom";
+import {
+  compose,
+  withProps,
+  lifecycle,
+  withHandlers,
+  pure,
+  withState
+} from "recompose";
 
-import { getUserManager, oidcLog } from '../Services';
-import CallbackComponent from './Callback.component';
+import { getUserManager, oidcLog } from "../Services";
+import CallbackComponent from "./Callback.component";
 
 export const onRedirectSuccess = ({ history }) => user => {
-  oidcLog.info('Successfull Callback');
+  oidcLog.info("Successfull Callback");
   if (user.state.url) {
     history.push(user.state.url);
   } else {
-    oidcLog.warn('no location in state');
+    oidcLog.warn("no location in state");
   }
 };
 
 export const onRedirectError = ({ history }) => error => {
   const { message } = error;
-  oidcLog.error(`There was an error handling the token callback: ${error.message}`);
+  oidcLog.error(
+    `There was an error handling the token callback: ${error.message}`
+  );
   history.push(`/authentication/not-authenticated?message=${message}`);
 };
 
 export const componentDidMountFunction = async props => {
   try {
-    const user = await props.userManager.signinRedirectCallback();
+    let user = await props.userManager.getUser();
+    if (!user || !user.expired) {
+      user = await props.userManager.signinRedirectCallback();
+    }
     props.onRedirectSuccess(user);
   } catch (error) {
     props.onRedirectError(error);
@@ -31,14 +43,14 @@ export const componentDidMountFunction = async props => {
 const withLifeCycle = lifecycle({
   async componentDidMount() {
     componentDidMountFunction(this.props);
-  },
+  }
 });
 
 const wrapUserManager = () => ({ userManager: getUserManager() });
 
 export const withCallbackHandlers = withHandlers({
   onRedirectSuccess,
-  onRedirectError,
+  onRedirectError
 });
 
 const enhance = compose(
