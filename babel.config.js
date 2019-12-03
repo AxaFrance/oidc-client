dd34fe03a6eb711c83d612a9fff931fc09bfa636
Olivier YOUF <olivier.youf@axa.fr>
2019-12-03 14:25:57 +0100

    ci(packages): Bump babel

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = api => {
   api.cache.never();
   return {
-    presets: ['@babel/env', '@babel/react'],
+    presets: ['@babel/preset-env', '@babel/preset-react'],
     plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-class-properties'],
   };
 };
