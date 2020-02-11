6fb6e48319291719c80646a1c1b6b4a50f510f3d
guillaume chervet <guillaume.chervet@gmail.com>
2020-02-11 15:08:07 +0100

    fix(fetchToken): options override

--- packages/fetch-core/src/fetchToken.spec.js
+++ packages/fetch-core/src/fetchToken.spec.js
@@ -2,24 +2,50 @@ import { fetchWithToken, fetchWrapper } from './fetchToken';
 
 describe('redux-fetch.fetchToken', () => {
   it('should fetchWithToken', async () => {
-    const getAccessToken = jest.fn();
+    const getAccessToken = jest.fn(() => 'test-token');
     const response = { status: 200 };
     const fetch = () =>
       new Promise(resolve => {
         resolve(response);
       });
-    const has = () => false;
-    const set = (key, value) => console.log(key + value);
+    const headers = {};
+    const has = key => key in headers;
+    const set = (key, value) => {
+      headers[key] = value;
+    };
     global.Headers = () => ({
       has,
       set,
     });
     const fetchResponse = await fetchWithToken(fetch, getAccessToken)('/a-url');
+    expect(headers.Accept).toBe('application/json');
+    expect(headers.Authorization).toBe('Bearer test-token');
     expect(fetchResponse).toBe(response);
+  });
+
+  it('should fetchWithToken and injected headers', async () => {
+    const getAccessToken = jest.fn(() => 'test-token');
+    const response = { status: 200 };
+    let receivedOptions = null;
+    const fetch = (url, options) =>
+        new Promise(resolve => {
+          receivedOptions = options;
+          resolve(response);
+        });
+    const headers = {};
+    const has = key => key in headers;
+    const set = (key, value) => {
+      headers[key] = value;
+    };
+    global.Headers = () => ({
+      has,
+      set,
+    });
 
     const fetchResponseWithCustomHeader = await fetchWithToken(fetch, getAccessToken)('/a-url', {
-      customHeader: 'customHeader',
+      credentials: 'include',
     });
+    expect(receivedOptions.credentials).toBe('include');
     expect(fetchResponseWithCustomHeader).toBe(response);
   });
 
