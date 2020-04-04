a78b77f468d7a154fa1033266973442de0b07a3c
Vincent Ricard <magic@magicninja.org>
2020-04-04 21:48:05 +0200

    Overrides the react-redux dependency

--- examples/redux/config-overrides.js
+++ examples/redux/config-overrides.js
@@ -4,5 +4,6 @@ module.exports = function override(config, env) {
   config.resolve.alias['react'] = require.resolve('react');
   config.resolve.alias['react-router'] = require.resolve('react-router');
   config.resolve.alias['react-router-dom'] = require.resolve('react-router-dom');
+  config.resolve.alias['react-redux'] = require.resolve('react-redux');
   return config;
 };
