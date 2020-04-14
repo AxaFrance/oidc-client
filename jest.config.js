9fc36cb7f07569e49b041acd07b89f1ca3285f46
Olivier YOUF <olivier.youf@axa.fr>
2020-04-14 09:36:00 +0200

    ci : updating yaml conf

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   cacheDirectory: '.cache/jest',
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    '@axa-fr/react-oidc-context': '<rootDir>/packages/context/dist',
-    '@axa-fr/react-oidc-core': '<rootDir>/packages/core/dist',
-    '@axa-fr/react-oidc-fetch-core': '<rootDir>/packages/fetch-core/dist',
-  },
   roots: ['<rootDir>/packages'],
   transform: {
     '^.+\\.(j|t)sx?$': 'babel-jest',
@@ -18,6 +11,5 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./scripts/jest.init.js'],
   setupFiles: ['raf/polyfill'],
-  testURL: 'http://localhost',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
