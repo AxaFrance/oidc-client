vimal2612/react-oidc

fix: add logout action

packages/context/src/oidcContext/AuthenticationContext.tsx
@@ -9,6 +9,7 @@ export type oidcContext = {
   events: UserManagerEvents;
   authenticating: ComponentType;
   isLoading: boolean;
+  isLogout: boolean;
   userManager: UserManager;
   error: string;
 };

packages/context/src/reactServices/OidcSecure.tsx
@@ -33,17 +33,17 @@ export const useOidcSecure = (
   isRequireAuthenticationInternal: typeof isRequireAuthentication,
   WrappedComponent: ComponentType
 ): ComponentType => {
-  const { isEnabled, oidcUser, authenticating } = useContext(AuthenticationContext);
+  const { isEnabled, oidcUser, authenticating, isLogout } = useContext(AuthenticationContext);
   useEffect(() => {
     oidcLogInternal.info('Protection : ', isEnabled);
-    if (isEnabled) {
+    if (isEnabled && !isLogout) {
       oidcLogInternal.info('Protected component mounted');
       authenticateUserInternal(userManager, location, history)();
     }
     return () => {
       oidcLogInternal.info('Protected component unmounted');
     };
-  }, [isEnabled, authenticateUserInternal, userManager, oidcLogInternal, location, history]);
+  }, [isEnabled, authenticateUserInternal, userManager, oidcLogInternal, location, history, isLogout]);
   const requiredAuth = useMemo(() => isRequireAuthenticationInternal(oidcUser, false) && isEnabled, [
     isEnabled,
     isRequireAuthenticationInternal,

AxaFrance/oidc-client

packages/context/src/oidcContext/AuthenticationContext.tsx
@@ -9,6 +9,7 @@ export type oidcContext = {
   events: UserManagerEvents;
   authenticating: ComponentType;
   isLoading: boolean;
+  isLoggingOut: boolean;
   userManager: UserManager;
   error: string;
 };

packages/context/src/reactServices/OidcSecure.tsx
@@ -33,17 +33,17 @@ export const useOidcSecure = (
   isRequireAuthenticationInternal: typeof isRequireAuthentication,
   WrappedComponent: ComponentType
 ): ComponentType => {
-  const { isEnabled, oidcUser, authenticating } = useContext(AuthenticationContext);
+  const { isEnabled, oidcUser, authenticating, isLoggingOut } = useContext(AuthenticationContext);
   useEffect(() => {
     oidcLogInternal.info('Protection : ', isEnabled);
-    if (isEnabled) {
+    if (isEnabled && !isLoggingOut) {
       oidcLogInternal.info('Protected component mounted');
       authenticateUserInternal(userManager, location, history)();
     }
     return () => {
       oidcLogInternal.info('Protected component unmounted');
     };
-  }, [isEnabled, authenticateUserInternal, userManager, oidcLogInternal, location, history]);
+  }, [isEnabled, authenticateUserInternal, userManager, oidcLogInternal, location, history, isLoggingOut]);
   const requiredAuth = useMemo(() => isRequireAuthenticationInternal(oidcUser, false) && isEnabled, [
     isEnabled,
     isRequireAuthenticationInternal,