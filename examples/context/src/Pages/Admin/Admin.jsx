e87c6eb4ddacb1f7256e9607140afce4d92db4a6
Olivier YOUF <olivier.youf@axa.fr>
2020-05-20 10:26:50 +0200

    fix(context): Memoize the child component

--- examples/context/src/Pages/Admin/Admin.jsx
+++ examples/context/src/Pages/Admin/Admin.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-import { withOidcUser, OidcSecure } from "@axa-fr/react-oidc-context";
+import React from 'react';
+import { withOidcUser, OidcSecure } from '@axa-fr/react-oidc-context';
 
-const Admin = ({ oidcUser }) => (
-  <OidcSecure>
-    <h1>Admin</h1>
-    <p>Protected Admin</p>
-    {oidcUser && <p>Bonjour {oidcUser.profile.name}</p>}
-  </OidcSecure>
-);
+const Admin = ({ oidcUser }) => {
+
+  return (
+    <>
+      <h1>Admin</h1>
+      <p>Protected Admin</p>
+      {oidcUser && <p>Bonjour {oidcUser.profile.name}</p>}
+    </>
+  );
+};
 
 // adding the oidc user in the props
 export default withOidcUser(Admin);
--- examples/context/src/Router/Routes.js
+++ examples/context/src/Router/Routes.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { withOidcSecure } from '@axa-fr/react-oidc-context';
+import { withOidcSecure , OidcSecure} from '@axa-fr/react-oidc-context';
 import Home from '../Pages/Home';
 import Dashboard from '../Pages/Dashboard';
 import Admin from '../Pages/Admin';
 
-const PageNotFound = () => (
-  <div>Page not found</div>
-);
+const PageNotFound = () => <div>Page not found</div>;
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route path="/dashboard" component={withOidcSecure(Dashboard)} />
-    <Route path="/admin" component={Admin} />
+    <Route path="/admin">
+      <OidcSecure>
+        <Admin />
+      </OidcSecure>
+    </Route>
     <Route path="/home" component={Home} />
     <Route component={PageNotFound} />
   </Switch>
