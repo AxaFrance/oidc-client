bf2ee89c0c33288c30b9662b93b15e7f473f83d2
Grallinger, Max <Max.grallinger@de-gmbh.com>
2020-07-17 13:17:48 +0200

    Bugfix: Popup now opens again if it was closed

--- packages/core/src/services/oidcServices.ts
+++ packages/core/src/services/oidcServices.ts
@@ -17,15 +17,16 @@ export const authenticateUser = (
   user: User = null
 ) => async (isForce: boolean = false, callbackPath: string = null) => {
   var usePopup = false;
-  if(userManager.settings != null && userManager.settings.popup_redirect_uri != null){
+  if (userManager.settings != null && userManager.settings.popup_redirect_uri != null) {
     usePopup = true;
   }
-  oidcLog.info('Use Popup: '+usePopup);
+  oidcLog.info('Use Popup: ' + usePopup);
   let oidcUser = user;
   if (!oidcUser) {
     oidcUser = await userManager.getUser();
   }
   if (userRequested) {
+    oidcLog.info('User is already requested. No new request will be sent.');
     return;
   }
   numberAuthentication++;
@@ -34,11 +35,17 @@ export const authenticateUser = (
   if (isRequireSignin(oidcUser, isForce)) {
     oidcLog.info('authenticate user...');
     userRequested = true;
-    if(usePopup){
-      await userManager.signinPopup({ data: { url } });
-    }else{
+
+    if (usePopup) {
+      try {
+        await userManager.signinPopup({ data: { url } });
+      } catch (e) {
+        userRequested = false;
+      }
+
+    } else {
       await userManager.signinRedirect({ data: { url } });
-    }    
+    }
     userRequested = false;
   } else if (oidcUser && oidcUser.expired) {
     userRequested = true;
@@ -46,12 +53,18 @@ export const authenticateUser = (
       await userManager.signinSilent();
     } catch (error) {
       if (numberAuthentication <= 1) {
-        if(usePopup){
-          await userManager.signinPopup({ data: { url } });
-        }else{
+
+        if (usePopup) {
+          try {
+            await userManager.signinPopup({ data: { url } });
+          } catch (e) {
+            userRequested = false;
+          }
+
+        } else {
           await userManager.signinRedirect({ data: { url } });
         }
-        
+
       } else {
         userRequested = false;
         oidcLog.warn(`session lost ${error.toString()}`);
@@ -62,43 +75,7 @@ export const authenticateUser = (
   }
 };
 
-export const authenticateUserPopup = (
-  userManager: UserManager,
-  location: Location,
-  history?: ReactOidcHistory,
-  user: User = null
-) => async (isForce: boolean = false, callbackPath: string = null) => {
-  let oidcUser = user;
-  if (!oidcUser) {
-    oidcUser = await userManager.getUser();
-  }
-  if (userRequested) {
-    return;
-  }
-  numberAuthentication++;
-  const url = callbackPath || location.pathname + (location.search || '');
 
-  if (isRequireSignin(oidcUser, isForce)) {
-    oidcLog.info('authenticate user...');
-    userRequested = true;
-    await userManager.signinPopup({ data: { url } });
-    userRequested = false;
-  } else if (oidcUser && oidcUser.expired) {
-    userRequested = true;
-    try {
-      await userManager.signinSilent();
-    } catch (error) {
-      if (numberAuthentication <= 1) {
-        await userManager.signinPopup({ data: { url } });
-      } else {
-        userRequested = false;
-        oidcLog.warn(`session lost ${error.toString()}`);
-        history.push(`/authentication/session-lost?path=${encodeURI(url)}`);
-      }
-    }
-    userRequested = false;
-  }
-};
 
 export const logoutUser = async (userManager: UserManager) => {
   if (!userManager || !userManager.getUser) {
