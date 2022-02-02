761c1dfb542bf5662384c22da66a62ecac18ae6c
guillaume chervet <guillaume.chervet@gmail.com>
2022-02-02 17:43:09 +0100

    remove redux

--- packages/redux/src/index.ts
+++
@@ -1,7 +0,0 @@
-export { OidcProvider, loadUser, reducer } from 'redux-oidc';
-
-export { default as AuthenticationCallback } from './AuthenticationCallback';
-
-export { default as Oidc } from './Oidc';
-export { default as OidcSecure, oidcSecure } from './OidcSecure';
-export { InMemoryWebStorage } from '@axa-fr/react-oidc-core';
