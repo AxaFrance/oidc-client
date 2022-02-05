97b7baa9b067f580c3b732c19b9c7e6214681ff6
Guillaume Chervet <guillaume.chervet@gmail.com>
2022-02-05 17:31:48 +0100

    update

--- packages/context/public/OidcServiceWorker.js
+++ packages/context/public/OidcServiceWorker.js
@@ -38,26 +38,45 @@ const handleFetch = async (event) => {
     
     if(originalRequest.url.startsWith(refreshTokenUrl)) {
         if (tokens != null) {
-            const newRequest = event.request.text().then(actualBody => {
+            /*const newRequest = originalRequest.text().then(actualBody => {
                 console.log("actualBody")
                 console.log(actualBody)
-                const newBody = actualBody.replace('%24(refresh_token)', tokens.refresh_token)
+                const newBody = actualBody.replace('REFRESH_TOKEN_SECURED_BY_OIDC_SERVICE_WORKER', tokens.refresh_token)
                 return new Request(originalRequest, {
                     body: newBody,
                     headers: {
                         ...originalRequest.headers,
-                        authorization: "Bearer " + tokens.access_token
                     }
                 });
             });
-            event.waitUntil(event.respondWith(fetch(newRequest)));
+            event.waitUntil(event.respondWith(fetch(originalRequest)));*/
+            const newfecth =originalRequest.text().then(actualBody => {
+                console.log("actualBody")
+                console.log(actualBody)
+                const newBody = actualBody.replace('REFRESH_TOKEN_SECURED_BY_OIDC_SERVICE_WORKER', encodeURIComponent(tokens.refresh_token))
+                return fetch(originalRequest, {
+                    body: newBody,
+                    method: originalRequest.method,
+                    headers: {
+                        ...originalRequest.headers,
+                        'Content-Type':'application/x-www-form-urlencoded'
+                    },
+                    mode: originalRequest.mode,
+                    cache: originalRequest.cache,
+                    redirect: originalRequest.redirect,
+                    referrer: originalRequest.referrer,
+                    credentials: originalRequest.credentials,
+                    integrity: originalRequest.integrity
+                });
+            });
+            event.waitUntil(event.respondWith(newfecth));
         } else {
             const response = fetch(event.request).then(async (response) => {
                 console.log('response: ', response);
                 tokens = await response.json()
                 console.log('response.body: ', tokens);
 
-                const secureTokens = {...tokens, access_token: "$(access_token)", refresh_token: "$(refresh_token)"};
+                const secureTokens = {...tokens, access_token: "ACCESS_TOKEN_SECURED_BY_OIDC_SERVICE_WORKER", refresh_token: "REFRESH_TOKEN_SECURED_BY_OIDC_SERVICE_WORKER"};
                 const body = JSON.stringify(secureTokens)
                 const newResponse = new Response(body, response)
                 return newResponse;
--- packages/context/src/oidc/vanilla/oidc.ts
+++ packages/context/src/oidc/vanilla/oidc.ts
@@ -9,9 +9,9 @@ import {
     FetchRequestor, LocalStorageBackend, DefaultCrypto
 } from '@openid/appauth';
 import {NoHashQueryStringUtils} from './noHashQueryStringUtils';
-//import {initAsync} from './initWorker'
+import {initAsync} from './initWorker'
 
-//initAsync("/OidcServiceWorker.js")
+initAsync("/OidcServiceWorker.js")
 /*
 
  class MemoryStorageBackend {
@@ -74,7 +74,7 @@ const autoRenewTokensAsync = async (oidc,refreshToken, intervalSeconds) =>{
         oidc.tokens = tokens;
         oidc.publishEvent(Oidc.eventNames.token_renewed, {});
         oidc.timeoutId = autoRenewTokensAsync(oidc, tokens.refreshToken, tokens.expiresIn)
-      }, (intervalSeconds- 20) *1000);
+      }, (intervalSeconds- 70) *1000);
 }
 
 const userInfoAsync = async (oidc)=> {
