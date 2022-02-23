e764a5d6305d611599f678ab6a897030e1519ee7
Guillaume Chervet <guillaume.chervet@gmail.com>
2022-02-23 08:16:45 +0100

    Update

--- packages/context/src/configurations.ts
+++ packages/context/src/configurations.ts
@@ -1,6 +1,6 @@
 export const configurationIdentityServer = {
     client_id: 'interactive.public.short',
-    redirect_uri: window.location.origin+'/authentication/callback',
+    redirect_uri: window.location.origin+'/authentication/callback', // http://localhost:4200/authentication/callback
     scope: 'openid profile email api offline_access',
     authority: 'https://demo.identityserver.io',
     refresh_time_before_tokens_expiration_in_second: 70,
@@ -10,7 +10,7 @@ export const configurationIdentityServer = {
 
 export const configurationAuth0 = {
     client_id: 'xGZxEAJhzlkuQUlWl90y1ntIX-0UDWHx',
-    redirect_uri: 'http://localhost:4200/callback',
+    redirect_uri: window.location.origin+'/callback', // http://localhost:4200/callback
     scope: 'openid profile email api offline_access',
     authority: 'https://kdhttps.auth0.com',
     refresh_time_before_tokens_expiration_in_second: 70,
