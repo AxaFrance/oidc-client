e29fcb92c7e1730c6f33f6f4e8ab8dd842ef6a54
Guillaume Chervet <guillaume.chervet@gmail.com>
2022-03-11 22:42:04 +0100

    fix(callbacksuccess): was not displayed

--- packages/context/src/oidc/core/default-component/index.ts
+++ packages/context/src/oidc/core/default-component/index.ts
@@ -1,5 +1,5 @@
 export { default as Authenticating } from './Authenticating.component';
-export { default as Callback } from './Callback.component';
+export { default as Callback, CallBackSuccess } from './Callback.component';
 export { default as SessionLost } from './SessionLost.component';
 export { default as Loading } from './Loading.component';
 export { default as AuthenticateError } from './AuthenticateError.component';
