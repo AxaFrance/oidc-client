f4cf7cc6d1991f693084111abbe832418be47c1e
guillaume chervet <guillaume.chervet@gmail.com>
2022-03-21 16:26:28 +0100

    fix(refreshtokens): wrong event name

--- packages/context/src/oidc/vanilla/oidc.ts
+++ packages/context/src/oidc/vanilla/oidc.ts
@@ -84,7 +84,7 @@ const autoRenewTokensAsync = async (oidc, refreshToken, expiresAt) => {
     return  timer.setTimeout(async () => {
         const currentTimeUnixSecond = new Date().getTime() /1000;
         const timeInfo = { timeLeft:((expiresAt - refreshTimeBeforeTokensExpirationInSecond)- currentTimeUnixSecond)};
-        oidc.publishEvent(Oidc.eventNames.token_renewed, timeInfo);
+        oidc.publishEvent(Oidc.eventNames.token_timer, timeInfo);
         if(currentTimeUnixSecond > (expiresAt - refreshTimeBeforeTokensExpirationInSecond)) {
             const tokens = await oidc.refreshTokensAsync(refreshToken);
             oidc.tokens= await setTokensAsync(oidc.serviceWorker, tokens);
