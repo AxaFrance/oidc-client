f9b54efc48876487773639b4d778c8fb3263fc8f
Guillaume Chervet <52236059+guillaume-chervet@users.noreply.github.com>
2022-03-31 10:38:04 +0200

    fix(OidcProvider): missing removeEventSubscription (#742)

--- packages/context/src/App.tsx
+++ packages/context/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
           <a className="navbar-brand" href="/">@axa-fr/react-oidc-context</a>
           <button className="navbar-toggler" type="button" onClick={() => setShow(!show)} data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"/>
           </button>
           <div style={show ? { display: "block" } : { display: 'none' }} className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
