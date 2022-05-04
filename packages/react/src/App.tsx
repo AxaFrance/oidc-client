316fa6bd412dd577ac2991f1fb744e79c3eb6360
Guillaume Chervet <guillaume.chervet@gmail.com>
2022-05-04 22:28:57 +0200

    update

--- packages/react/src/App.tsx
+++ packages/react/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Home } from "./Home";
 import { Profile, SecureProfile } from "./Profile";
 import { configurationAuth0, configurationIdentityServer, configurationIdentityServerWithoutDiscovery } from './configurations';
