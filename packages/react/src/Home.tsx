03099227462fd4365f54b7d5650fac0555f5266a
Guillaume Chervet <guillaume.chervet@gmail.com>
2022-05-10 18:15:42 +0200

    update

--- packages/react/src/Home.tsx
+++ packages/react/src/Home.tsx
@@ -12,7 +12,8 @@ export const Home = () => {
                     <h5 className="card-title">Home</h5>
                     <p className="card-text">React Demo Application protected by OpenId Connect. More info on about oidc on <a href="https://github.com/AxaGuilDEv/react-oidc">GitHub @axa-fr/react-oidc</a> </p>
                     {!isAuthenticated && <button type="button" className="btn btn-primary" onClick={() => login('/profile')}>Login</button>}
-                    {isAuthenticated && <button type="button" className="btn btn-primary" onClick={() => logout('/profile')}>logout</button>}
+                    {isAuthenticated && <p><button type="button" className="btn btn-primary" onClick={() => logout('/profile')}>logout /profile</button></p>}
+                    {isAuthenticated && <p><button type="button" className="btn btn-primary" onClick={() => logout('')}>logout </button></p>}
                 </div>
             </div>
         </div>
