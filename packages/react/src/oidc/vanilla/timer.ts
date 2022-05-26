b6f7989fbfa6e2ed5ce06359448cc4101a8d72d8
Tomasz Bogdal <tomek@cledar.pl>
2022-05-26 13:41:37 +0200

    feat(nextjs-compatibility): Fixed timmer running in NodeJS during Server Side Rendering in Next JS apps

--- packages/react/src/oidc/vanilla/timer.ts
+++ packages/react/src/oidc/vanilla/timer.ts
@@ -90,11 +90,15 @@
     }());
 
     if (!workerPort) {
+        // In NextJS with SSR (Server Side Rendering) during rending in Node JS, the window object is undefined,
+        // the global object is used instead as it is the closest approximation of a browsers window object.
+        const bindContext = (typeof window === 'undefined')? global: window;
+
         return {
-            setTimeout: setTimeout.bind(window),
-            clearTimeout: clearTimeout.bind(window),
-            setInterval: setInterval.bind(window),
-            clearInterval: clearInterval.bind(window)
+            setTimeout: setTimeout.bind(bindContext),
+            clearTimeout: clearTimeout.bind(bindContext),
+            setInterval: setInterval.bind(bindContext),
+            clearInterval: clearInterval.bind(bindContext)
         };
     }
 
