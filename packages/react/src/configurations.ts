3246b8164c846b355a27d40068aba56c2ad6c7f8
Guillaume Chervet <guillaume.chervet@gmail.com>
2022-06-10 10:09:50 +0200

    test

--- packages/react/src/configurations.ts
+++ packages/react/src/configurations.ts
@@ -10,7 +10,7 @@ export const configurationIdentityServer = {
   };
 
 export const configurationIdentityServerWithoutDiscovery = {
-    client_id: 'interactive.public.short',
+    client_id: 'interactive.public',
     redirect_uri: window.location.origin+'/authentication/callback',
     //silent_redirect_uri: window.location.origin+'/authentication/silent-callback',
     scope: 'openid profile email api offline_access',
