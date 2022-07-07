811cdeee35ad3ed26e22173b8ea9fe2470a713a4
Guillaume Chervet <52236059+guillaume-chervet@users.noreply.github.com>
2022-07-07 06:52:29 +0200

    Update configurations.ts

--- packages/react/src/configurations.ts
+++ packages/react/src/configurations.ts
@@ -1,12 +1,12 @@
 export const configurationIdentityServer = {
-    client_id: 'interactive.public',
+    client_id: 'interactive.public.short',
     redirect_uri: window.location.origin+'/authentication/callback',
     silent_redirect_uri: window.location.origin+'/authentication/silent-callback',
     scope: 'openid profile email api offline_access',
     authority: 'https://demo.duendesoftware.com',
-    refresh_time_before_tokens_expiration_in_second: 70,
-    service_worker_relative_url:'/OidcServiceWorker.js',
-    service_worker_only: false,
+   // refresh_time_before_tokens_expiration_in_second: 70,
+  //  service_worker_relative_url:'/OidcServiceWorker.js',
+  //  service_worker_only: false,
   };
 
 export const configurationIdentityServerWithHash = {
