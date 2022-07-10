1f2d10e3494ce5a47e99054c92faa7bf0ccdc9c7
Guillaume Chervet <guillaume.chervet@gmail.com>
2022-07-10 15:44:31 +0200

    update

--- packages/react/src/oidc/core/routes/route-utils.spec.ts
+++ packages/react/src/oidc/core/routes/route-utils.spec.ts
@@ -1,11 +1,15 @@
 import { getPath } from './route-utils';
 
-it('getPath should return the full path of an url', () => {
-  const path1 = getPath('http://example.com/pathname');
-  const path2 = getPath('http://example.com:3000/pathname/?search=test#hash');
-  const path3 = getPath('http://example.com:3000/pathname/#hash?search=test');
 
-  expect(path1).toEqual('/pathname');
-  expect(path2).toEqual('/pathname#hash');
-  expect(path3).toEqual('/pathname#hash');
-});
+test.each([['http://example.com/pathname', '/pathname'], 
+  ['http://example.com:3000/pathname/?search=test#hash', '/pathname#hash'], 
+  ['http://example.com:3000/pathname/#hash?search=test', '/pathname#hash'], 
+  ['http://example.com:3000/pathname#hash?search=test', '/pathname#hash'], 
+  ['http://example.com:3000/', ''],])(
+    'getPath should return the full path of an url (%i)',
+    (uri, expected) => {
+
+      const path = getPath(uri);
+      expect(path).toBe(expected);
+    },
+);
