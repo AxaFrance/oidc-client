c63b3bff1c69b855935d0ec97115b88694877f5e
Guillaume Chervet <guillaume.chervet@gmail.com>
2022-07-17 23:45:26 +0200

    update

--- packages/react/src/configurations.ts
+++ packages/react/src/configurations.ts
@@ -7,9 +7,9 @@ export const configurationIdentityServer = {
     authority: 'https://demo.duendesoftware.com',
     authority_time_cache_wellknowurl_in_second: 60* 60,
     refresh_time_before_tokens_expiration_in_second: 260,
-    //service_worker_relative_url:'/OidcServiceWorker.js',
-    //service_worker_only: false, 
-    storage: localStorage,
+    service_worker_relative_url:'/OidcServiceWorker.js',
+    service_worker_only: false, 
+    //storage: localStorage,
     //silent_signin_timeout: 3333000
     monitor_session:true,
   };
