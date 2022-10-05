e080638126f54e3977ceec9219f3db5c2ca025c6
Mike Mao <Mike.Mao@NZFUNDS.CO.NZ>
2022-10-05 20:32:37 +1300

    added typed OidcUserInfo

--- packages/react/src/oidc/User.ts
+++ packages/react/src/oidc/User.ts
@@ -9,8 +9,40 @@ export enum OidcUserStatus {
 }
 
 export type OidcUser = {
-    user: any,
+    user: OidcUserInfo,
     status: OidcUserStatus
+}
+
+export interface OidcUserInfo {
+    sub?: string;
+    name?: string;
+    given_name?: string;
+    family_name?: string;
+    middle_name?: string;
+    nickname?: string;
+    preferred_username?: string;
+    profile?: string;
+    picture?: string;
+    website?: string;
+    email?: string;
+    email_verified?: boolean;
+    gender?: string;
+    birthdate?: string;
+    zoneinfo?: string;
+    locale?: string;
+    phone_number?: string;
+    phone_number_verified?: boolean;
+    address?: OidcAddressClaim;
+    updated_at?: number;
+}
+
+export interface OidcAddressClaim {
+    formatted?: string;
+    street_address?: string;
+    locality?: string;
+    region?: string;
+    postal_code?: string;
+    country?: string;
 }
 
 export const useOidcUser = (configurationName="default") => {
