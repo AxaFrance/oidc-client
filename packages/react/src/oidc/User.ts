1a0ca52b78662e46ef82736600d8c5ea43812705
zyofeng <zyofeng@msn.com>
2022-10-07 04:40:23 +1300

    feat(oidc): Added typed OidcUserInfo (#875)
    * added typed OidcUserInfo
    
    * make sub mandatory
    
    * added generic overrides ofr OidcUserInfo
    
    Co-authored-by: Mike Mao <Mike.Mao@NZFUNDS.CO.NZ>

--- packages/react/src/oidc/User.ts
+++ packages/react/src/oidc/User.ts
@@ -8,13 +8,45 @@ export enum OidcUserStatus {
     LoadingError = 'Error loading user'
 }
 
-export type OidcUser = {
-    user: any,
+export type OidcUser<T extends OidcUserInfo = OidcUserInfo> = {
+    user: T,
     status: OidcUserStatus
 }
 
-export const useOidcUser = (configurationName="default") => {
-    const [oidcUser, setOidcUser] = useState<OidcUser>({user: null, status: OidcUserStatus.Unauthenticated});
+export interface OidcUserInfo {
+    sub: string;
+    name?: string;
+    given_name?: string;
+    family_name?: string;
+    middle_name?: string;
+    nickname?: string;
+    preferred_username?: string;
+    profile?: string;
+    picture?: string;
+    website?: string;
+    email?: string;
+    email_verified?: boolean;
+    gender?: string;
+    birthdate?: string;
+    zoneinfo?: string;
+    locale?: string;
+    phone_number?: string;
+    phone_number_verified?: boolean;
+    address?: OidcAddressClaim;
+    updated_at?: number;
+}
+
+export interface OidcAddressClaim {
+    formatted?: string;
+    street_address?: string;
+    locality?: string;
+    region?: string;
+    postal_code?: string;
+    country?: string;
+}
+
+export const useOidcUser = <T extends OidcUserInfo = OidcUserInfo>(configurationName="default") => {
+    const [oidcUser, setOidcUser] = useState<OidcUser<T>>({user: null, status: OidcUserStatus.Unauthenticated});
 
     const oidc = VanillaOidc.get(configurationName);
     useEffect(() => {
