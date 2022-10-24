2c668975e3a3501165c78107e2401df3816dc3b8
Jason Finch <jason.finch@gmail.com>
2022-10-24 23:19:35 +1000

    feat(Exports): Export OidcConfiguration type

--- packages/react/src/oidc/index.ts
+++ packages/react/src/oidc/index.ts
@@ -3,4 +3,5 @@ export { OidcProvider } from './OidcProvider';
 export { OidcSecure, withOidcSecure } from './OidcSecure';
 export { useOidc, useOidcAccessToken, useOidcIdToken } from './ReactOidc';
 export { OidcUserStatus, useOidcUser } from './User';
+export type { OidcConfiguration } from './vanilla/oidc';
 export { TokenRenewMode } from './vanilla/parseTokens';
