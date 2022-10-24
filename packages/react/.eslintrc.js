00adf7ebf87b971916edfac7ae4b7a91c0404c77
Jason Finch <jason.finch@gmail.com>
2022-10-24 23:19:16 +1000

    fix(eslint): Add config for eslint for all files in src.

--- packages/react/.eslintrc.js
+++ packages/react/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: ['./config/defaultEslintConfig'], 
     parserOptions: {
-      project: './tsconfig.json',
+      project: './tsconfig.eslint.json',
       tsconfigRootDir: __dirname,
     },
     rules: {
