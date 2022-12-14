df0ab3f1413050485c50c93ef42bca354b998301
Guillaume Chervet <guillaume.chervet@gmail.com>
2022-12-14 12:11:38 +0100

    feat(oidc): accept multiple protocol (#920)
    * feat(oidc): accept multiple protocol
    * fix lint
    * add error test case

--- packages/react/src/oidc/vanilla/route-utils.spec.ts
+++ packages/react/src/oidc/vanilla/route-utils.spec.ts
@@ -5,6 +5,8 @@ test.each([['http://example.com/pathname', '/pathname'],
   ['http://example.com:3000/pathname/?search=test#hash', '/pathname#hash'], 
   ['http://example.com:3000/pathname/#hash?search=test', '/pathname#hash'], 
   ['http://example.com:3000/pathname#hash?search=test', '/pathname#hash'], 
+  ['capacitor://localhost/index.html', '/index.html'], 
+  ['capacitor://localhost/pathname#hash?search=test', '/pathname#hash'], 
   ['http://example.com:3000/', ''],])(
     'getPath should return the full path of an url',
     (uri, expected) => {
@@ -13,3 +15,7 @@ test.each([['http://example.com/pathname', '/pathname'],
       expect(path).toBe(expected);
     },
 );
+
+test('wrong uri format', () => {
+    expect(() => getPath("urimybad/toto.com")).toThrowError();
+});
