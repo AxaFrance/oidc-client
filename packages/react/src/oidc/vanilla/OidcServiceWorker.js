41df79318ca704b38fd39468604cb8cca7745288
Doug Copestake <doug@tukesoftware.com>
2023-02-23 20:37:05 +0000

    Reduce service worker keep-alive response to between 2.5 and 5 minutes maximum

--- packages/react/src/oidc/vanilla/OidcServiceWorker.js
+++ packages/react/src/oidc/vanilla/OidcServiceWorker.js
@@ -261,7 +261,7 @@ const keepAliveAsync = async (event) => {
     const init = { status: 200, statusText: 'oidc-service-worker' };
     const response = new Response('{}', init);
     if (!isFromVanilla) {
-        for (let i = 0; i < 240; i++) {
+        for (let i = 0; i < 150; i++) {
             await sleep(1000 + Math.floor(Math.random() * 1000));
             const cache = await caches.open('oidc_dummy_cache');
             await cache.put(event.request, response.clone());
