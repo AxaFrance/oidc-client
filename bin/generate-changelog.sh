82ed28183d4efea9c1835f4eed9a25c254ecd8a5
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-11-29 16:32:28 +0100

    Update generate-changelog.sh (release) (#1203)

--- bin/generate-changelog.sh
+++ bin/generate-changelog.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-CHANGELOG_FILE="CHANGELOG.md"
-echo "# Changelog" > $CHANGELOG_FILE
-echo "" >> $CHANGELOG_FILE
-
 REPO_URL="https://github.com/AxaFrance/oidc-client"
+FILE="README.md"
 
-git log -n 100 --pretty=format:"%h%x09%an%x09%ad%x09%s%d" --date=short | while IFS= read -r line
+echo "# README" > $FILE # create the readme file or overwrite if exist
+
+tags=$(git tag) # get all tags
+
+for tag in $tags
 do
-read -r commit_hash commit_author commit_date commit_message remainder <<<"$line"
-if [[ $remainder = *tag:* ]]; then
-if [ ! -z "$tag" ]; then
-echo -e "\n## $tag" >> $CHANGELOG_FILE
-fi
-tag=$(echo "$remainder" | grep -o "tag: [v.*[0-9]]*" | awk '{print $2}' | tr -d '(),')
-echo -e "\n## $tag" >> $CHANGELOG_FILE
-fi
-# Generer un lien en bonne et due forme pour chaque commit
-echo "- [$commit_message]($REPO_URL/commit/$commit_hash) - by $commit_author on $commit_date" >> $CHANGELOG_FILE
+  echo "## $tag" >> $FILE # write tag as title
+  commits=$(git log $tag --pretty="%h") # get the list of commits for this tag
+  
+  for commit in $commits
+  do
+     echo "- [$commit]($REPO_URL/commit/$commit)" >> $FILE # write each commit as a list item with link to the commit
+  done
 done
