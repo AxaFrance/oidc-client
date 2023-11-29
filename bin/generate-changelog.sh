edb89b390eefce93dbaa02c813d388bbaf1088e2
Guillaume Chervet <guillaume.chervet@gmail.com>
2023-11-29 15:32:18 +0100

    fix (release)

--- bin/generate-changelog.sh
+++ bin/generate-changelog.sh
@@ -15,7 +15,7 @@ git log -n 100 --pretty=format:'{"tag":"%d", "hash":"%h", "message":"%s", "autho
 jq -r 'select(.tag != null) | .tag |= sub("tag: "; "") | .tag + "," + .hash + "," + .message + "," + .author + "," + .date' | \
 while IFS=',' read -r tag commit_hash commit_message commit_author commit_date; do
     if [ ! -z "$tag" ]; then
-        echo -e "\n## $tag"
+        echo -e "\n## $tag" >> $CHANGELOG_FILE
     fi
-    echo "- [$commit_message]('$REPO_URL'/commit/$commit_hash) ($commit_author, $commit_date)"
-done >> $CHANGELOG_FILE = $2; next} {commit_hash=$1; commit_message=$2; commit_author=$3; commit_date=$4; print "- [" commit_message "]('"$REPO_URL"/commit/" commit_hash "') (" commit_author ", " commit_date ")"} END {if (tag) {print "\n## " tag}}' >> $CHANGELOG_FILE
+    echo "- [$commit_message]('$REPO_URL'/commit/$commit_hash) ($commit_author, $commit_date)" >> $CHANGELOG_FILE
+done
