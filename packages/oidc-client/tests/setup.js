91f055efa0c6d148f4e048c05daea2c950ea913a
Guillaume Chervet <guillaume.chervet@gmail.com>
2024-02-06 09:39:50 +0100

    upf

--- packages/oidc-client/tests/setup.js
+++ packages/oidc-client/tests/setup.js
@@ -1,7 +1,12 @@
-﻿import { cleanup } from '@testing-library/react';
-import { afterEach,expect } from 'vitest';
+﻿import { configDefaults } from 'vitest/config'
+import {defineConfig} from "vite";
 
-// runs a cleanup after each test case (e.g. clearing jsdom)
-afterEach(() => {
-    cleanup();
+export default defineConfig({
+    //plugins: [react(), tsconfigPaths()],
+    test: {
+        exclude:[
+            ...configDefaults.exclude,
+            'public/*'
+        ]
+    },
 });
