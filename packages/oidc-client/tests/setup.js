9ccb12b65ec1e0ab8cf19bd4f81687a92cbff6f6
Guillaume Chervet <guillaume.chervet@gmail.com>
2024-02-06 09:47:56 +0100

    test (alpha)

--- packages/oidc-client/tests/setup.js
+++ packages/oidc-client/tests/setup.js
@@ -2,7 +2,6 @@
 import {defineConfig} from "vite";
 
 export default defineConfig({
-    //plugins: [react(), tsconfigPaths()],
     test: {
         exclude:[
             ...configDefaults.exclude,
