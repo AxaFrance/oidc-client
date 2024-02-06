9b2f150bbe2a7d7ccb82dbe38c6f2649a77f3228
Guillaume Chervet <guillaume.chervet@gmail.com>
2024-02-06 11:25:49 +0100

    update

--- packages/react-oidc/tests/setup.js
+++ packages/react-oidc/tests/setup.js
@@ -1,5 +1,5 @@
 ﻿import { cleanup } from '@testing-library/react';
-import { afterEach,expect } from 'vitest';
+import { afterEach } from 'vitest';
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
