73eae7e9d68dea6ebec0ed82ba7873941c0e371e
Jason Finch <jason.finch@gmail.com>
2024-07-11 05:26:19 +1000

    refactor(oidcServiceWorker): Extract GetCurrentDatabaseTokenEndpoint, add tests. (#1405) (release)

---
+++ packages/oidc-client-service-worker/src/oidcConfig.ts
@@ -0,0 +1,17 @@
+import { Database, OidcConfig } from './types';
+import { normalizeUrl } from './utils';
+
+const getMatchingOidcConfigurations = (database: Database, url: string): OidcConfig[] => {
+	return Object.values(database).filter((config) => {
+		const { oidcServerConfiguration } = config || {};
+		const { tokenEndpoint, revocationEndpoint } = oidcServerConfiguration || {};
+
+		const normalizedUrl = normalizeUrl(url);
+		return (
+			(tokenEndpoint && normalizedUrl.startsWith(normalizeUrl(tokenEndpoint))) ||
+			(revocationEndpoint && normalizedUrl.startsWith(normalizeUrl(revocationEndpoint)))
+		);
+	});
+};
+
+export { getMatchingOidcConfigurations as getCurrentDatabasesTokenEndpoint };
