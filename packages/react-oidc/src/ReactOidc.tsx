23ae6ba287a85cc879c92228feb3855dcbe2930e
ShimpeiKamiguchi <134356973+ShimpeiKamiguchi@users.noreply.github.com>
2024-08-02 16:17:40 +0900

    fix(oidc): Add undefined as union type into arguments for login, logout, renewTokens (#1429) (alpha)
    * Add null as union type into arguments for login, logout, renewTokens functions
    
    * Update default value as undefined for ayncronous function

--- packages/react-oidc/src/ReactOidc.tsx
+++ packages/react-oidc/src/ReactOidc.tsx
@@ -46,7 +46,7 @@ export const useOidc = (configurationName = defaultConfigurationName) => {
 
   const login = (
     callbackPath: string | undefined = undefined,
-    extras: StringMap | null = null,
+    extras: StringMap | undefined = undefined,
     silentLoginOnly = false,
   ) => {
     return getOidc(configurationName).loginAsync(
@@ -59,12 +59,12 @@ export const useOidc = (configurationName = defaultConfigurationName) => {
   };
   const logout = (
     callbackPath: string | null | undefined = undefined,
-    extras: StringMap | null = null,
+    extras: StringMap | undefined = undefined,
   ) => {
     return getOidc(configurationName).logoutAsync(callbackPath, extras);
   };
   const renewTokens = async (
-    extras: StringMap | null = null,
+    extras: StringMap | undefined = undefined,
   ): Promise<OidcAccessToken | OidcIdToken> => {
     const tokens = await getOidc(configurationName).renewTokensAsync(extras);
 
