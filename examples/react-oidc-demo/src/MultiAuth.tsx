3881746f99c9ace5dc4eded95a2a012e7633d78e
Guillaume Chervet <guillaume.chervet@gmail.com>
2024-11-05 18:37:01 +0100

    feat(demo): better demo

--- examples/react-oidc-demo/src/MultiAuth.tsx
+++ examples/react-oidc-demo/src/MultiAuth.tsx
@@ -56,6 +56,7 @@ const MultiAuth = ({ configurationName, handleConfigurationChange }) => {
           </p>
           <select value={configurationName} onChange={handleConfigurationChange}>
             <option value="config_classic">config_classic</option>
+            <option value="config_with_monitor_session">config_classic_monitor_session</option>
             <option value="config_without_refresh_token">config_without_refresh_token</option>
             <option value="config_without_silent_login">config_without_silent_login</option>
             <option value="config_without_refresh_token_silent_login">
@@ -150,6 +151,9 @@ export const MultiAuthContainer = () => {
       redirect_uri: callBack,
       silent_redirect_uri: '',
       scope: 'openid profile email api offline_access',
+    },
+    config_with_monitor_session: {
+      monitor_session: true,
     },
     config_without_refresh_token_silent_login: {
       ...configurationIdentityServer,
