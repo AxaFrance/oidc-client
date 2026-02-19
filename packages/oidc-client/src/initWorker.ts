7cda494b880b2daaddb4ecab7c44feaf353041cb
Mérill Téterel <59962715+mteterel@users.noreply.github.com>
2026-02-19 23:55:22 +0100

    Remove stale userInfo data when using Service Worker
    This is a follow-up of the fix I pushed in https://github.com/AxaFrance/oidc-client/pull/1603. The initial fix only worked in a non-SW scenario, the issue is still present otherwise.

--- packages/oidc-client/src/initWorker.ts
+++ packages/oidc-client/src/initWorker.ts
@@ -247,6 +247,10 @@ export const initWorkerAsync = async (
   }
 
   const clearAsync = async status => {
+    // Ensure no stale userInfo data is kept in the browser storage when using SW.
+    // This completes the initial fix from https://github.com/AxaFrance/oidc-client/pull/1603
+    delete sessionStorage[`oidc.${configurationName}.userInfo`];
+    
     return sendMessageAsync(registration)({ type: 'clear', data: { status }, configurationName });
   };
 
